6e050bf78db6d2b2852ffd35cb36ecb534529fa7
Constantine <kabelomestnykh@edu.hse.ru>
2024-02-25 18:47:23 +0300

    Users service version 1 complete

---
+++ users.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "commontypes.h"
+#include "customservice.h"
+#include <grpcpp/grpcpp.h>
+#include "protofiles/users.pb.h"
+
+using grpc::Channel;
+
+using namespace tinkoff::public_::invest::api::contract::v1;
+
+class TINKOFFINVESTSDK_EXPORT Users : public CustomService {
+public:
+    Users(std::shared_ptr<Channel> channel, const std::string &token);
+    ~Users();
+
+    // Метод получения счетов пользователя: тело запроса — GetAccountsRequest, тело ответа — GetAccountsResponse
+    ServiceReply GetAccounts();
+    // Расчёт маржинальных показателей по счёту: тело запроса — GetMarginAttributesRequest, тело ответа — GetMarginAttributesResponse
+    ServiceReply GetMarginAttributes(const std::string &accountId);
+    // Запрос тарифа пользователя: тело запроса — GetUserTariffRequest, тело ответа — GetUserTariffResponse
+    ServiceReply GetUserTariff();
+    // Метод получения информации о пользователе: тело запроса — GetInfoRequest, тело ответа — GetInfoResponse
+    ServiceReply GetInfo();
+
+private:
+    std::unique_ptr<UsersService::Stub> m_usersService;
+};
