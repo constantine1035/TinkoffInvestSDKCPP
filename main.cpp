c1c66b01e588ffe87632bd8fa6a9821cbae6cd2b
Мухин Яков Александрович <yaamukhin_1@edu.hse.ru>
2024-04-24 16:50:20 +0300

    Maybe correct include boost+openssl.

--- main.cpp
+++ main.cpp
@@ -1,46 +1,22 @@
 #include <iostream>
-#include "client.h"
-//#include "services/users.h"
-#include "services/sandbox.h"
-
-#include <grpc++/grpc++.h>
-
-using tinkoff_invest_sdk_cpp_sandbox_service::Sandbox;
-
-// 3021000
-
-using grpc::Channel;
+#include "include/xxhr/xxhr/xxhr.hpp"
 
 int main()
 {
 //    printf("123");
 //    InvestApiClient client("invest-public-api.tinkoff.ru:443", "t.9DjFbOC06vcgI5xplPO-NkKMUWHjIy8eamhr1rmdVt5YoDMYYWArqCZyrD_oGodNQ");
 //    InvestApiClient client("invest-public-api.tinkoff.ru:443", "t.xkHLcK7d-Bjz8YVC2UCnI6nOU0FQoXAVC0BI7S4hsCGMnJdOQtFB3QYVEv4xGnNH2fZ_cSLY6QaInRLcPSMayg");
-    InvestApiClient client("sandbox-invest-public-api.tinkoff.ru:443", "t.oCCIjlXTnrRcIvWV8-W9c92ZVPwQbUyGuSfbX2l3a8O_X0VLKGJEWeIWoH2b0lhkveSPzRuZ355hlErOexHDkA");
+//    InvestApiClient client("sandbox-invest-public-api.tinkoff.ru:443", "t.oCCIjlXTnrRcIvWV8-W9c92ZVPwQbUyGuSfbX2l3a8O_X0VLKGJEWeIWoH2b0lhkveSPzRuZ355hlErOexHDkA");
 
-    auto sandbox = std::dynamic_pointer_cast<Sandbox>(client.Service("sandbox"));
+    using namespace xxhr;
 
-    auto res = sandbox->OpenSandboxAccount("qwerty");
-    std::cout << res.GetStatus().ok() << std::endl;
-    std::cout << res.GetErrorMessage() << std::endl;
+    GET( "http://httpbin.org/anything"s,
+        on_response = [](auto&& resp) {
 
+            std::cout << resp.text;
 
-    //get references to users service
-//    auto users = std::dynamic_pointer_cast<Users>(client.Service("users"));
-
-    //print all opened accounts id
-//    auto accounts = users->GetAccounts();
-//    std::cout << accounts.AccountCount();
-//    for (int i = 0; i < accounts.AccountCount(); i++) {
-//        std::cout << accounts.AccountID(i) << accounts.AccountName(i) << std::endl;
-//    }
-
-
-    //print info about your account
-//    auto accountId = accounts.accountID(0);
-//    auto portfolio = sandbox->GetSandboxPortfolio(accountId, PortfolioRequest_CurrencyRequest_RUB);
-//    std::cout << portfolio.ptr()->DebugString() << std::endl;
-
+        }
+    );
 
     return 0;
 }
